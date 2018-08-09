5815618b9282c8052f7d234443d6cff38866afed
Minh Nguyen <minh.nguyen@watasolutions.com>
2018-08-09 18:32:16 +0700

    Update Branch API

--- common/models/branch.js
+++ common/models/branch.js
@@ -1,5 +1,28 @@
 'use strict';
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+  provider: 'google',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: 'AIzaSyCPCdvGsSBIuTuh9ATjMZCojZn6VHSW5Qo', // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+
+var geocoder = NodeGeocoder(options);
 
 module.exports = function(Branch) {
+  Branch.observe('before save', function(ctx, next) {
+    if(ctx.isNewInstance) {
+      geocoder.geocode(ctx.instance.address, function(err, res) {
+        ctx.instance.location = {
+          latitude: res[0].latitude,
+          longitude: res[0].longitude,
+        };
 
+        next();
+      })
+    }
+  })
 };
