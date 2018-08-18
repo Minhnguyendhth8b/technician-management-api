5c6f20774355657c3fe4afe811294abdd672304a
Minh Nguyen <minh.nguyen@watasolutions.com>
2018-08-18 09:38:47 +0700

    Update Notification

--- common/models/notification.js
+++ common/models/notification.js
@@ -93,10 +93,15 @@ module.exports = function(Notification) {
   });
 
   Notification.seen = (options, next) => {
-    const {currentMember, currentStore} = options;
+    const {currentMember} = options;
+    console.log(currentMember.id);
     Notification.updateAll({
       "data.memberId": currentMember.id.toString()
-    }, {status: 1}, next);
+    }, {status: 1}, (err, notifications) => {
+      if(err) return next(err);
+      console.log(notifications);
+      return next(null, notifications);
+    });
   };
 
   Notification.countUnreadMessages = (options, next) => {
